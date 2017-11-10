3b7af1ffdd4970ccf0a360c5694fb945ee3d2ea8
ErikHH <erik.hoogeveen@outlook.com>
2017-11-10 08:56:08 +0100

    It works, but still too slow to my taste.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mosaic Me!
+
+How to start the Mosaic Me! application
+---
+
+1. Run `mvn clean install` to build your application
+1. Start application with `java -jar target/mosaic-me-1.0-SNAPSHOT.jar server config.yml`
+1. To check that your application is running enter url `http://localhost:8080`
+
+Health Check
+---
+
+To see your applications health enter url `http://localhost:8081/healthcheck`
--- routes/mosaic.js
+++ routes/mosaic.js
@@ -11,10 +11,15 @@ var upload = multer()
 
 /* GET mosaic of me. */
 router.get('/:x/:y/:z', function(req, res, next) {
-    console.log(req.params);
-    var img = fs.readFileSync('./public/images/sample.jpg');
-    res.writeHead(200, {'Content-Type': 'image/jpeg' });
-    res.end(img, 'binary');
+    //console.log(req.params);
+    findMostRecentMosaicFile(config.mosaicDir)
+        .then(function(pictureGrid) {
+            var tile = calculateTile(pictureGrid, config.minTileDimension, req.params.x, req.params.y, req.params.z);
+            mosaic.getTile(pictureGrid, tile.dimensions, tile.x1, tile.y1, tile.x2, tile.y2, config, function(err, mosaicContext) {
+                res.writeHead(200, {'Content-Type': 'image/jpeg' });
+                streamImage(res, mosaicContext);
+            });
+        });
 });
 
 /* POST a new image to mosaic with */
@@ -41,6 +46,95 @@ image_index.loadIndex(config, function(err) {
         console.error(err);
     }
 });
+
+const streamImage = function(res, context) {
+    try {
+        var stream = context.canvas.createJPEGStream();
+        stream.on('data', function(chunk) {
+            res.write(chunk, 'binary');
+        }).on('end', function() {
+         res.end();
+        }).on('error', function(err) {
+            console.log("Save error: " + err);
+            res.send(err);
+        });
+    } catch (err) {
+        res.send(err);
+    }
+}
+
+const calculateTile = function(mosaic, minTileDimension, x, y, z) {
+    var tile = {dimensions: {width: 0, height: 0}, x1: 0, y1: 0, x2: 0, y2: 0};
+    tile.dimensions.width = timesTimesTwo(minTileDimension.width, z - 1);
+    tile.dimensions.height = timesTimesTwo(minTileDimension.height, z - 1);
+
+    var tileGridWidth = timesDivideTwo(mosaic.grid.width, z - 1);
+    var tileGridHeight = timesDivideTwo(mosaic.grid.height, z - 1);
+    
+    tile.x1 = x  * tileGridWidth;
+    tile.y1 = y * tileGridHeight;
+    tile.x2 = tile.x1 + tileGridWidth;
+    tile.y2 = tile.y1 + tileGridHeight;
+    
+    return tile;
+}
+
+const timesTimesTwo = function(val, timesTwo) {
+    var result = val;
+    for(var i = 0; i < timesTwo; i++) {
+        result = result * 2;
+    }
+    return result;
+}
+
+const timesDivideTwo = function(val, divdeTwo) {
+    var result = val;
+    for(var i = 0; i < divdeTwo; i++) {
+        result = result / 2;
+    }
+    return result;
+}
+
+const findMostRecentMosaicFile = function(mosaicDir) {
+    return new Promise(function(result, reject) {
+        fs.readdir(mosaicDir, function(err, files) {
+            if (err) {
+                reject(err);
+            }
+            var promises = [];
+            for (var index = 0; index < files.length; index++) {
+                if (files[index].endsWith('.json')) {
+                    promises.push(getCreationTime(path.join(mosaicDir, files[index])));
+                }
+            }
+            if (promises.length <= 0) {
+                reject("No mosaic files found");
+            }
+            Promise.all(promises).then((creationTimes) => {
+                creationTimes.sort(function(lh, rh) {
+                    return rh.createTime - lh.createTime;
+                });
+                fs.readFile(creationTimes[0].file, function(err, data) {
+                    if (err) {
+                        reject(err);
+                    }
+                    result(JSON.parse(data));
+                });
+            });
+        });
+    });
+}
+
+const getCreationTime = function(file) {
+    return new Promise(function (resolve, reject) {
+        fs.stat(file, function(err, stats) {
+            if (err) {
+                reject(err);
+            }
+            resolve({file: file, createTime: stats.ctime});
+        });
+    });
+}
 
 module.exports = router;
 
